77f6ffdd2bb78c69c546d00e3a2f1acf4a467371
RomanHlodann <roman.hlodann@gmail.com>
2024-05-12 17:52:49 +0300

    add Route model where source and destination are fk Airport

--- airport/models.py
+++ airport/models.py
@@ -5,3 +5,16 @@ class Airport(models.Model):
     name = models.CharField(max_length=255)
     closest_big_city = models.CharField(max_length=100)
 
+
+class Route(models.Model):
+    source = models.ForeignKey(
+        Airport,
+        on_delete=models.CASCADE,
+        related_name="source_routes"
+    )
+    destination = models.ForeignKey(
+        Airport,
+        on_delete=models.CASCADE,
+        related_name="destination_routes"
+    )
+    distance = models.IntegerField()
