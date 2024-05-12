7ce834024570562b4e6756ab2e0b0fa5e5ca1f67
RomanHlodann <roman.hlodann@gmail.com>
2024-05-12 22:38:10 +0300

    changes in paramaters naming in Route serializers

--- airport/serializers.py
+++ airport/serializers.py
@@ -25,24 +25,23 @@ class RouteSerializer(serializers.ModelSerializer):
 
 
 class RouteListSerializer(serializers.ModelSerializer):
-    source_closest_big_city = serializers.CharField(
+    source = serializers.CharField(
         source="source.closest_big_city"
     )
-    destination_closest_big_city = serializers.CharField(
+    destination = serializers.CharField(
         source="destination.closest_big_city"
     )
 
     class Meta:
         model = Route
-        fields = ("id",  "source_closest_big_city",
-                  "destination_closest_big_city", "distance")
+        fields = ("id",  "source",
+                  "destination", "distance")
 
 
-class RouteDetailSerializer(RouteListSerializer):
-    source_airport = serializers.CharField(source="source.name")
-    destination_airport = serializers.CharField(source="destination.name")
+class RouteDetailSerializer(serializers.ModelSerializer):
+    source = AirportSerializer(many=False, read_only=True)
+    destination = AirportSerializer(many=False, read_only=True)
 
     class Meta:
         model = Route
-        fields = ("id", "source_airport", "source_closest_big_city",
-                  "destination_airport", "destination_closest_big_city", "distance")
+        fields = ("id", "source", "destination", "distance")
