2d2115cc65d3b830ec862571f576e3fe3a40d748
RomanHlodann <roman.hlodann@gmail.com>
2024-05-12 22:40:24 +0300

    add urls for Airport and Route viewsets

---
+++ airport/urls.py
@@ -0,0 +1,15 @@
+from django.urls import path, include
+from rest_framework import routers
+
+from airport.views import (
+    AirportViewSet,
+    RouteViewSet
+)
+
+router = routers.DefaultRouter()
+router.register("all", AirportViewSet)
+router.register("routes", RouteViewSet)
+
+urlpatterns = [path("", include(router.urls))]
+
+app_name = "airport_service"
