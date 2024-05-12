90247a2fd981f6f289c29f2380cecfc858443a8c
RomanHlodann <roman.hlodann@gmail.com>
2024-05-12 22:39:40 +0300

    add viewsets for Airport and Route models

--- airport/views.py
+++ airport/views.py
@@ -1,3 +1,35 @@
-from django.shortcuts import render
+from rest_framework import viewsets
 
-# Create your views here.
+from airport.models import (
+    Airport,
+    Route,
+    Crew,
+    AirplaneType,
+    Airplane,
+    Flight,
+    Order,
+    Ticket
+)
+from airport.serializers import (
+    AirportSerializer,
+    RouteSerializer,
+    RouteListSerializer,
+    RouteDetailSerializer
+)
+
+
+class AirportViewSet(viewsets.ModelViewSet):
+    queryset = Airport.objects.all()
+    serializer_class = AirportSerializer
+
+
+class RouteViewSet(viewsets.ModelViewSet):
+    queryset = Route.objects.select_related("source", "destination")
+    serializer_class = RouteSerializer
+
+    def get_serializer_class(self):
+        if self.action == "list":
+            return RouteListSerializer
+        if self.action == "retrieve":
+            return RouteDetailSerializer
+        return self.serializer_class
