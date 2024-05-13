0063a1f49982af7c3ea987515e239057240a54e7
RomanHlodann <roman.hlodann@gmail.com>
2024-05-13 17:53:58 +0300

    add urls for displaying static files

--- airport_service/urls.py
+++ airport_service/urls.py
@@ -17,9 +17,12 @@ Including another URLconf
 
 from django.contrib import admin
 from django.urls import path, include
+from django.conf.urls.static import static
+from django.conf import settings
+
 
 urlpatterns = [
     path("admin/", admin.site.urls),
     path("api/airport/", include("airport.urls", namespace="airport")),
-    path("api/user/", include("user.urls", namespace="user"))
-]
+    path("api/users/", include("user.urls", namespace="user"))
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
