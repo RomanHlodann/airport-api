334b2c0359db0ceaed8eed4886d2fe4a6c4ecd58
RomanHlodann <roman.hlodann@gmail.com>
2024-05-13 15:01:11 +0300

    add serializers for Order model

--- airport/serializers.py
+++ airport/serializers.py
@@ -134,3 +134,23 @@ class FlightDetailSerializer(FlightSerializer):
         model = Flight
         fields = ("id", "route", "airplane", "departure_time",
                   "arrival_time", "crew", "taken_places")
+
+
+class OrderSerializer(serializers.ModelSerializer):
+    tickets = TicketSerializer(many=True, read_only=False, allow_empty=False)
+
+    class Meta:
+        model = Order
+        fields = ("id", "tickets", "created_at")
+
+    def create(self, validated_data):
+        with transaction.atomic():
+            tickets_data = validated_data.pop("tickets")
+            order = Order.objects.create(**validated_data)
+            for ticket_data in tickets_data:
+                Ticket.objects.create(order=order, **ticket_data)
+            return order
+
+
+class OrderListSerializer(OrderSerializer):
+    tickets = TicketListSerializer(many=True, read_only=True)
